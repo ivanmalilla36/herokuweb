3c7a99163323984645c7108ea7d57b54b1b07ac4
ivanmalilla36 <ivanmalilla36@gmail.com>
2018-10-09 15:03:10 -0600

    se agrego el lisener del puerto

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
-const port = 3678;
+const port = .listen(process.env.PORT || 3678);
 
 const path = require("path")
 
